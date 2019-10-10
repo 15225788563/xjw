1406f818ecd192eb2f2fbdaf9c0b4ad7e976e29d
wys <1970577662@qq.com>
2019-10-10 10:17:43 +0800

    写了个首页

---
+++ pages/home/home.js
@@ -0,0 +1,119 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    search: "/imgs/2.png",
+    img:"/imgs/3.png",
+    nav:[
+        {
+          "url":"/imgs/1.png",
+          "name":"筐子管理"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "库存管理"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "应收应付"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "货物采购"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "货物销售"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "客户管理"
+        },
+        {
+          "url": "/imgs/1.png",
+          "name": "发起需求"
+        },
+    ],
+    contact:[
+      {
+        "url": "/imgs/6.png",
+        "name": "李光洙-盐城"
+      },
+      {
+        "url": "/imgs/6.png",
+        "name": "张晶晶-盐城"
+      },
+      {
+        "url": "/imgs/6.png",
+        "name": "王思-盐城"
+      },
+      {
+        "url": "/imgs/6.png",
+        "name": "王思-盐城"
+      },
+      {
+        "url": "/imgs/6.png",
+        "name": "王思-盐城"
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
